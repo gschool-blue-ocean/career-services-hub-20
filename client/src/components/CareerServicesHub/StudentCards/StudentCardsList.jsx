3ec14315baf7db3afb132d9c06bf128be0e6cc64
andmoo71 <andrewmooree@gmail.com>
2023-05-18 09:45:47 -0700

    new student input now renders to page!

--- client/src/components/CareerServicesHub/StudentCards/StudentCardsList.jsx
+++ client/src/components/CareerServicesHub/StudentCards/StudentCardsList.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { useEffect } from "react";
 import { StudentsContext } from "../../../context/studentsContext";
 import AddStudent from "../AddStudentCard/AddStudentCard";
 import StudentCard from "./StudentCard";
@@ -24,15 +25,10 @@ export default function StudentCardslist({
 }) {
   const studentContext = useContext(StudentsContext);
   const students = studentContext.studentsData;
-  const [newStudent, setNewStudent] = useState({
-    cohort: "",
-    tscm_id: 0,
-    student_first: "",
-    student_last: "",
-    college_degree: "",
-    sec_clearance: "",
-  });
-
+  const [currentStudents, setCurrentStudents] = useState(students);
+  useEffect(() => {
+    setCurrentStudents(students);
+  }, [students]);
   const filteredStudents = filterStudents(
     students,
     currentCohort,
@@ -53,14 +49,20 @@ export default function StudentCardslist({
   );
 
   function handleUpdateNewStudent(newStudentObj) {
-    setNewStudent(newStudentObj)
-    console.log('I am the handleUpdateNewStudent')
+    const newCurrentStudents = currentStudents.push(newStudentObj);
+    setCurrentStudents(newCurrentStudents);
+    console.log("new student obj:", newStudentObj);
+    console.log("current students:", currentStudents);
+    console.log("new current students:", newCurrentStudents);
   }
 
   return (
     <>
       <div className="student-card-container">
-        <AddStudent filterStudents={filterStudents} handleUpdateNewStudent={handleUpdateNewStudent} />
+        <AddStudent
+          filterStudents={filterStudents}
+          handleUpdateNewStudent={handleUpdateNewStudent}
+        />
         {filterStudents != null
           ? filteredStudents.map((student) => {
               {
