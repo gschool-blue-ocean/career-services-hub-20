7077b791811f623ba4ce8f13aec188fb796370cd
Never404s <kdsossa@gmail.com>
2023-05-25 11:18:32 -0500

    final kevin

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import pg from 'pg';
 import jwt from 'jsonwebtoken'
-import NodeCache from 'node-cache'
 
 const { Pool } = pg;
 
@@ -27,30 +26,19 @@ app.use(
 
 // --------------------------------------------- STUDENT ROUTES ----------------------------------------------------------------------------
 
-const cache = new NodeCache();
-
 app.get("/students", async (req, res, next) => {
-  // Check if the data is cached.
+  // Fetch data from the database.
   try {
-    // Check if the data is cached.
-    const cachedData = cache.get("students");
-    if (cachedData) {
-      // If the data is available in the cache, send it directly.
-      res.send(cachedData);
-    } else {
-      // If the data is not cached, fetch it from the database.
-      const results = await db.query(`SELECT student.*, service_manager.tscm_first, service_manager.tscm_last
-                                      FROM student 
-                                      JOIN service_manager ON service_manager.tscm_id = student.tscm_id`);
+    const results = await db.query(`
+      SELECT student.*, service_manager.tscm_first, service_manager.tscm_last
+      FROM student 
+      JOIN service_manager ON service_manager.tscm_id = student.tscm_id
+    `);
 
-      // Cache the data for 30 seconds.
-      cache.set("students", results.rows, 30);
-
-      // Send the data.
-      res.send(results.rows);
-    }
-  } catch (error) {
-    next(error);
+    // Send the data.
+    res.send(results.rows);
+  } catch (err) {
+    next(err); // Pass errors to the error handler
   }
 });
 
