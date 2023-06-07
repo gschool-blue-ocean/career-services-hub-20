7589e83374d37941402dd48703783891cbbc6a10
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-06-07 10:41:40 -1000

    updated testing script in App.test.jsx

--- client/src/components/App/App.test.jsx
+++ client/src/components/App/App.test.jsx
@@ -20,46 +20,55 @@ beforeAll(() => {
 afterAll(() => {
   server.close();
 });
-
-it("displays tasks from the api", async () => {
-  server.use(
-    rest.get("/api/tasks", (req, res, ctx) => {
-      return res(
-        ctx.json([
-          { id: 1, description: "Do the dishes" },
-          { id: 2, description: "Mow the lawn" },
-        ])
-      );
-    })
-  );
-  const { findByText } = render(<App />);
-  await findByText("Do the dishes");
-  await findByText("Mow the lawn");
+ 
+it('test always passes', async () => {
+  expect(true).toBe(true);
 });
 
-it("deletes a task when clicked", async () => {
-  server.use(
-    rest.get("/api/tasks", (req, res, ctx) => {
-      return res(ctx.json([{ id: 3, description: "Do the dishes" }]));
-    })
-  );
-  const { findByText } = render(<App />);
-  const spy = vi.fn();
-
-  await findByText("Do the dishes");
-  server.use(
-    rest.delete("/api/tasks/3", (req, res, ctx) => {
-      spy();
-      return res(ctx.status(204));
-    })
-  );
-  server.use(rest.get("/api/tasks", (req, res, ctx) => res(ctx.json([]))));
-
-  const deleteButton = await findByText("X");
-
-  userEvent.click(deleteButton);
-
-  await findByText("No Tasks Remaining");
-
-  expect(spy).toHaveBeenCalled();
+it('test always fails', async () => {
+  expect(true).toBe(false);
 });
+
+
+// it("displays tasks from the api", async () => {
+//   server.use(
+//     rest.get("/api/tasks", (req, res, ctx) => {
+//       return res(
+//         ctx.json([
+//           { id: 1, description: "Do the dishes" },
+//           { id: 2, description: "Mow the lawn" },
+//         ])
+//       );
+//     })
+//   );
+//   const { findByText } = render(<App />);
+//   await findByText("Do the dishes");
+//   await findByText("Mow the lawn");
+// });
+
+// it("deletes a task when clicked", async () => {
+//   server.use(
+//     rest.get("/api/tasks", (req, res, ctx) => {
+//       return res(ctx.json([{ id: 3, description: "Do the dishes" }]));
+//     })
+//   );
+//   const { findByText } = render(<App />);
+//   const spy = vi.fn();
+
+//   await findByText("Do the dishes");
+//   server.use(
+//     rest.delete("/api/tasks/3", (req, res, ctx) => {
+//       spy();
+//       return res(ctx.status(204));
+//     })
+//   );
+//   server.use(rest.get("/api/tasks", (req, res, ctx) => res(ctx.json([]))));
+
+//   const deleteButton = await findByText("X");
+
+//   userEvent.click(deleteButton);
+
+//   await findByText("No Tasks Remaining");
+
+//   expect(spy).toHaveBeenCalled();
+// });
