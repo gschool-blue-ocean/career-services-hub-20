99dcdd18d2526a47f2c61049c66c91a47c7cbf45
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-06-07 13:16:39 -1000

    automated testing updated w/linting

--- client/src/components/App/App.test.jsx
+++ client/src/components/App/App.test.jsx
@@ -8,19 +8,63 @@ import App from "./App.jsx";
 
 const server = setupServer();
 
-afterEach(() => {
-  cleanup();
-  server.resetHandlers();
-});
+// Add the request handlers
 
-beforeAll(() => {
-  server.listen();
-});
+// Handler for GET /api/tasks
+// server.use(
+//   rest.get("students", (req, res, ctx) => {
+//     return res(
+//       ctx.json(
+//         {
+//           "student_id": 1,
+//           "student_first": "Johnathon",
+//           "student_last": "Pfannerstill",
+//           "cohort": "MCSP-19",
+//           "sec_clearance": "Undetermined",
+//           "career_status": "Searching",
+//           "course_status": "Graduate",
+//           "college_degree": "Masters Not in CS/STEM",
+//           "tscm_id": 2,
+//           "tscm_first": "Sarah",
+//           "tscm_last": "Weimann"
+//         }
+//       )
+//     );
+//   })
+// );
+// it("displays tasks from the api", async () => {
+//   const { findByText } = render(<App />);
+//   await findByText("Do the dishes");
+//   await findByText("Mow the lawn");
+// });
 
-afterAll(() => {
-  server.close();
-});
- 
+// // Handler for DELETE /api/tasks/3
+// server.use(
+//   rest.delete("/api/tasks/3", (req, res, ctx) => {
+//     return res(ctx.status(204));
+//   })
+// );
+
+// // Handler for subsequent GET /api/tasks
+// // server.use(
+// //   rest.get("/api/tasks", (req, res, ctx) => {
+// //     return res(ctx.json([]));
+// //   })
+// // );
+
+// afterEach(() => {
+//   cleanup();
+//   server.resetHandlers();
+// });
+
+// beforeAll(() => {
+//   server.listen();
+// });
+
+// afterAll(() => {
+//   server.close();
+// });
+
 it('test always passes', async () => {
   expect(true).toBe(true);
 });
@@ -30,45 +74,14 @@ it('test always passes', async () => {
 // });
 
 
-// it("displays tasks from the api", async () => {
-//   server.use(
-//     rest.get("/api/tasks", (req, res, ctx) => {
-//       return res(
-//         ctx.json([
-//           { id: 1, description: "Do the dishes" },
-//           { id: 2, description: "Mow the lawn" },
-//         ])
-//       );
-//     })
-//   );
-//   const { findByText } = render(<App />);
-//   await findByText("Do the dishes");
-//   await findByText("Mow the lawn");
-// });
-
 // it("deletes a task when clicked", async () => {
-//   server.use(
-//     rest.get("/api/tasks", (req, res, ctx) => {
-//       return res(ctx.json([{ id: 3, description: "Do the dishes" }]));
-//     })
-//   );
 //   const { findByText } = render(<App />);
-//   const spy = vi.fn();
 
 //   await findByText("Do the dishes");
-//   server.use(
-//     rest.delete("/api/tasks/3", (req, res, ctx) => {
-//       spy();
-//       return res(ctx.status(204));
-//     })
-//   );
-//   server.use(rest.get("/api/tasks", (req, res, ctx) => res(ctx.json([]))));
-
 //   const deleteButton = await findByText("X");
 
 //   userEvent.click(deleteButton);
 
 //   await findByText("No Tasks Remaining");
 
-//   expect(spy).toHaveBeenCalled();
 // });
