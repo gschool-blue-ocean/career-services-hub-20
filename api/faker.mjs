00dae5cf32d3859d57d4fa729250319eadfed4c4
cnickbradford <cnickbradford@gmail.com>
2023-06-13 16:41:37 -0500

    refactored database to merge student and milestone database, reconfig the faker

--- api/faker.mjs
+++ api/faker.mjs
@@ -39,7 +39,7 @@ const cohorts = [
   "MCSP-21",
   "MCSP-22",
 ];
-const careerStatus = ["Searching", "Hired", "Not Started"];
+const careerStatus = ["Searching", "Hired", "Not Currently Searching"];
 const courseStatus = ["Student", "Graduate"];
 const secClearance = [
   "Undetermined",
@@ -92,6 +92,11 @@ const seedStudents = async () => {
       career_status: careerStatus[randomNumber], // Randomly pick a element in the self-defined careerStatus array
       course_status: courseStatus[randomNumber2], // Randomly pick a element in the self-defined courseStatus array
       college_degree: collegeDegree[randomNumber4], // Randomly pick a element in the self-defined collegeDegree array
+      cover_letter: progress_stat[Math.floor(Math.random() * 3)], // Randomly pick a element in the self-defined progress_stat array
+      resume: progress_stat[Math.floor(Math.random() * 3)], // Randomly pick a element in the self-defined progress_stat array
+      linkedin: progress_stat[Math.floor(Math.random() * 3)], // Randomly pick a element in the self-defined progress_stat array
+      personal_narrative: progress_stat[Math.floor(Math.random() * 3)], // Randomly pick a element in the self-defined progress_stat array
+      hunter_access: progress_stat[Math.floor(Math.random() * 3)], // Randomly pick a element in the self-defined progress_stat array
       tscm__id: faker.datatype.number({ min: 1, max: SEED_CAREER_MANAGER }), // Use faker method to create random number between 1 and MAX # of managers
     });
   }
@@ -99,8 +104,8 @@ const seedStudents = async () => {
   try {
     await db.query("TRUNCATE TABLE student CASCADE"); // DROP TABLES already in the SQL database
     await db.query("ALTER SEQUENCE student_student_id_seq RESTART WITH 1"); // Reset students entity primary key to 1
-    const queryString = `INSERT INTO student (student_first, student_last,student_email,student_password, cohort, sec_clearance, career_status, course_status, college_degree, tscm_id) 
-                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`;
+    const queryString = `INSERT INTO student (student_first, student_last,student_email,student_password, cohort, sec_clearance, career_status, course_status, college_degree, cover_letter, resume, linkedin, personal_narrative, hunter_access ,tscm_id) 
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *`;
 
     // For each student, query SQL database with INSERT statement to add student
     for (let i = 0; i < SEED_STUDENT_ROWS; i++) {
@@ -114,6 +119,11 @@ const seedStudents = async () => {
         career_status,
         course_status,
         college_degree,
+        cover_letter,
+        resume,
+        linkedin,
+        personal_narrative,
+        hunter_access,
         tscm__id,
       } = studentList[i];
       await db.query(queryString, [
@@ -126,6 +136,11 @@ const seedStudents = async () => {
         career_status,
         course_status,
         college_degree,
+        cover_letter,
+        resume,
+        linkedin,
+        personal_narrative,
+        hunter_access,
         tscm__id,
       ]);
     }
@@ -235,36 +250,9 @@ const seedCalendar = async () => {
   }
 };
 
-const seedMilestone = async () => {
-  let currentStudentNumber = 1;
-
-  try {
-    await db.query("TRUNCATE TABLE milestone CASCADE"); // DROP TABLES already in the SQL database
-    await db.query("ALTER SEQUENCE milestone_mile_id_seq RESTART WITH 1"); // Reset milestones entity primary key to 1
-    const queryString = `INSERT INTO milestone (mile_name, progress_stat, student_id) VALUES ($1, $2, $3) RETURNING *`;
-
-    // For every student inside database, have a nested for loop to create each milestone for each every student
-    for (let i = 0; i < SEED_STUDENT_ROWS; i++) {
-      for (let i = 0; i < studentMilestone.length; i++) {
-        let randomNumber = Math.floor(Math.random() * 3); // Randomly generate a number between 0-2
-        await db.query(queryString, [
-          studentMilestone[i],
-          progress_stat[randomNumber],
-          currentStudentNumber,
-        ]); // Insert milestone into SQL database
-      }
-      currentStudentNumber++; // Go to the next student
-    }
-    console.log("Milestone seeded successfully");
-  } catch (err) {
-    console.log("Error seeding Calendar", err);
-  }
-};
-
-// Seed the database in the order described below
 
 
 seedServiceManager().then(() =>
-  seedStudents().then(() => seedCalendar().then(() => seedMilestone()))
+  seedStudents()
 );
 
