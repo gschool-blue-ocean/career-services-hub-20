8d1826f3bf27a40560a77c64624a27ed80b81a65
jrm0527 <geeziegames@gmail.com>
2023-06-23 16:25:47 -0400

    fixes for student view on mobile

--- client/src/components/CareerServicesHub/StudentCards/StudentViewCard.jsx
+++ client/src/components/CareerServicesHub/StudentCards/StudentViewCard.jsx
@@ -63,7 +63,6 @@ const StudentViewCard = ({ popUpLogOff, handleLogOff, studentInfo, url }) => {
         {popUpLogOff > 0 ? (
           <div className="login-popup">
             Successfully logged off. Navigating in {popUpLogOff}s...
-
           </div>
         ) : null}
         <nav className="student-nav">
@@ -75,9 +74,19 @@ const StudentViewCard = ({ popUpLogOff, handleLogOff, studentInfo, url }) => {
               <a onClick={() => handleSend()}>Notification</a>
               <a onClick={handleLogOff}>Log Out</a>
             </div>
-            </div>
-         
+          </div>
         </nav>
+        <div style={{ display: "flex", overflow: "auto" }}>
+          <nav className="student-setting-nav">
+            <div style={{ paddingLeft: "50px" }}>
+              <h1>Settings</h1>
+              <hr />
+              <p>My Profile</p>
+              <p onClick={changeProfile}>Change My Profile</p>
+              <p onClick={() => handleSend()}>Notification</p>
+              <p onClick={handleLogOff}>Log Out</p>
+            </div>
+          </nav>
           <div className="student-profile-data">
             <div className="profile-background-img">
               <img src="https://th.bing.com/th/id/R.b173d064715990e210a19f080fde122a?rik=wyy2%2bsDxPMBAGA&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f3%2fe%2fc%2f563599.jpg&ehk=IIlQVDqUjTmsDGCECQSTU1ogn28Flsaf2OWi74E3Ubk%3d&risl=&pid=ImgRaw&r=0"></img>
@@ -85,10 +94,8 @@ const StudentViewCard = ({ popUpLogOff, handleLogOff, studentInfo, url }) => {
             <div className="profile-info">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-
                 height="0"
                 viewBox="0 0 448 448"
-
               >
                 <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
               </svg>
@@ -179,17 +186,6 @@ const StudentViewCard = ({ popUpLogOff, handleLogOff, studentInfo, url }) => {
                 </div>
               </div>
             </div>
-            {/* <div className="studentCard">wm
-            <div className='card-item'>{currentStudent.student_first} {currentStudent.student_last} </div>
-            <div className='card-item'>• Education Level: {currentStudent.college_degree} <p>• Personal Narrative: {currentStudent.personal_narrative} </p></div>
-            <div className='card-item'>• Cohort: {currentStudent.cohort}<p>• CSM: {currentStudent.tscm_first} {currentStudent.tscm_last}</p></div>
-            <div className='card-item'>• LinkedIn: {currentStudent.linkedin}<p>• Hunter Account: {currentStudent.hunter_access}</p></div>
-            <div className='card-item'>• Course Standing: {currentStudent.course_status} <p>• Security Clearance: {currentStudent.sec_clearance}</p></div>
-            <div className='card-item'>• Cover Letter: {currentStudent.cover_letter} <p>• Resume: {currentStudent.resume}</p></div>
-            <div className='contact'>
-            <div>Contact your TSCM: {currentStudent.tscm_email}</div>
-          </div> 
-        </div> */}
           </div>
         </div>
       </div>
